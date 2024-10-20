9c18fc6981698ef5262adce36a8c4d640cd59826
MaxMeulendijks <91459085+MaxMeulendijks@users.noreply.github.com>
2024-10-20 18:07:48 +0100

    Added obstacles + jumping

---
+++ Create with Code Part 2 Mission 1 - Sound and Effects/Prototype 3/Assets/Course Library/Scripts/SpawnManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPosition = new Vector3(25, 0, 0);
+    private float startDelay = 3;
+    private float repeatRate = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObstacle(){
+        Instantiate(obstaclePrefab, spawnPosition, obstaclePrefab.transform.rotation);
+    }
+}
