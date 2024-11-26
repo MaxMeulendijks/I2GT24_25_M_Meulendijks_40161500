0d9fcec41c03a05dd12340ca38859c16b449018f
MaxMeulendijks <91459085+MaxMeulendijks@users.noreply.github.com>
2024-11-26 19:09:00 +0000

    Add UI element when key found

--- Project/Camo Leo/Assets/Scripts/KeyFound.cs
+++ Project/Camo Leo/Assets/Scripts/KeyFound.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -7,6 +8,7 @@ public class KeyFound : MonoBehaviour
 {
 
     public PlayerController player;
+    public GameObject keyFoundIndicator;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +25,7 @@ public class KeyFound : MonoBehaviour
         if (collision.gameObject.CompareTag("Player")) {
             Destroy(gameObject);
             player.keyFound = true;
+            keyFoundIndicator.SetActive(true);
         }
     }
 }
